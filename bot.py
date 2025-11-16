ccca17120e2922c48cf4e89fbc66cc42618a8cbe
Derek Rodriguez <55155927+derekr3848@users.noreply.github.com>
2025-11-15 20:33:20 -0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -102,7 +102,8 @@ intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
 
-bot = commands.Bot(command_prefix="!", intents=intents)
+bot = commands.Bot(command_prefix="!", intents=intents, help_command=None)
+
 
 # --------------------------------------------------
 # Utility functions
