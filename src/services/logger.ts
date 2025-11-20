05fdaa9d24c3998e08ff1a754f1a1071d31000ee
Derek Rodriguez <55155927+derekr3848@users.noreply.github.com>
2025-11-20 12:46:45 -0600

    Update logger.ts

--- src/services/logger.ts
+++ src/services/logger.ts
@@ -1,1 +1,6 @@
+export const log = {
+  info: (...args: any[]) => console.log('[INFO]', ...args),
+  warn: (...args: any[]) => console.warn('[WARN]', ...args),
+  error: (...args: any[]) => console.error('[ERROR]', ...args)
+};
 
