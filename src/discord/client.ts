cca124b12ea586563e5a2eacbbdbd6bad2f51239
Derek Rodriguez <55155927+derekr3848@users.noreply.github.com>
2025-11-20 12:53:09 -0600

    Update client.ts

--- src/discord/client.ts
+++ src/discord/client.ts
@@ -1,1 +1,22 @@
+import { Client, GatewayIntentBits, Partials } from 'discord.js';
+import { env } from '../config/env';
+import { log } from '../services/logger';
+
+export const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.DirectMessages,
+    GatewayIntentBits.MessageContent
+  ],
+  partials: [Partials.Channel]
+});
+
+client.once('ready', () => {
+  log.info(`Logged in as ${client.user?.tag}`);
+});
+
+export async function startDiscordClient() {
+  await client.login(env.DISCORD_BOT_TOKEN);
+}
 
