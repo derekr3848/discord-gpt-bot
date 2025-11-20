0ba00c5d5cb42c963fc808b65f510652898cbc6c
Derek Rodriguez <55155927+derekr3848@users.noreply.github.com>
2025-11-20 13:36:50 -0600

    Create redisClient.ts

---
+++ src/memory/redisClient.ts
@@ -0,0 +1,9 @@
+import { createClient } from "redis";
+import { env } from "../config/env";
+
+export const redis = createClient({
+  url: env.REDIS_URL,
+  password: env.REDIS_PASSWORD
+});
+
+redis.connect().catch(console.error);
