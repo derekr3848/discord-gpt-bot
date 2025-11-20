f547e3d06f3afe94e56e764292a053f24a5c39f0
Derek Rodriguez <55155927+derekr3848@users.noreply.github.com>
2025-11-20 12:47:00 -0600

    Update redisClient.ts

--- src/services/redisClient.ts
+++ src/services/redisClient.ts
@@ -1,1 +1,12 @@
+import Redis from 'ioredis';
+import { env } from '../config/env';
+import { log } from './logger';
+
+export const redis = new Redis(env.REDIS_URL, {
+  password: env.REDIS_PASSWORD,
+  maxRetriesPerRequest: 3
+});
+
+redis.on('connect', () => log.info('Connected to Redis'));
+redis.on('error', (err) => log.error('Redis error', err));
 
