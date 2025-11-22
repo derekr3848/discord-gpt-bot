546f383328b8ddf98cfee59291992eab61238c15
angeline <145289718+ange-222@users.noreply.github.com>
2025-11-21 22:38:32 -0600

    Delete src/discord/commands/adminadminConfigService.ts

--- src/discord/commands/adminadminConfigService.ts
+++
@@ -1,6 +0,0 @@
-import { redis } from "../../memory/redisClient";
-
-export async function setGlobalConfig(key: string, value: any) {
-  await redis.set(`config:${key}`, JSON.stringify(value));
-  return true;
-}
