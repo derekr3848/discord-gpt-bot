238303df9841da4ca7e8924a1924d138d6a7ee1e
angeline <145289718+ange-222@users.noreply.github.com>
2025-11-21 22:52:02 -0600

    Update registerCommands.ts

--- src/registerCommands.ts
+++ src/registerCommands.ts
@@ -6,7 +6,6 @@ import path from "path";
 
 const commands: any[] = [];
 
-// 🔥 FIXED PATH
 const commandsPath = path.join(__dirname, "discord/commands");
 
 function loadCommands() {
@@ -37,7 +36,7 @@ const rest = new REST({ version: "10" }).setToken(env.DISCORD_BOT_TOKEN);
     console.log(`📡 Registering ${commands.length} slash commands...`);
 
     await rest.put(
-  Routes.applicationGuildCommands(env.DISCORD_CLIENT_ID!, env.DISCORD_GUILD_ID!)
+      Routes.applicationGuildCommands(env.DISCORD_CLIENT_ID!, env.DISCORD_GUILD_ID!),
       
       { body: commands }
     );
